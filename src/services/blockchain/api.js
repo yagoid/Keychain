830445a43b81d3308c5804ebb47ba2d3475972bf
Yago Iglesias Díaz <yago.igle@gmail.com>
2024-07-22 20:20:00 +0200

    update url in api.js

--- src/services/blockchain/api.js
+++ src/services/blockchain/api.js
@@ -1,6 +1,6 @@
 
 export const postData = async (endpoint, data) => {
-    const url = "http://backend:5000/" + endpoint
+    const url = "http://backend:8000/" + endpoint
     // const url = "http://192.168.1.87:5000/" + endpoint
     // const url = "http://localhost:5000/" + endpoint
 
@@ -14,7 +14,7 @@ export const postData = async (endpoint, data) => {
 };
 
 export const fetchData = async (endpoint) => {
-    const url = "http://backend:5000/" + endpoint
+    const url = "http://backend:8000/" + endpoint
     // const url = "http://192.168.1.87:5000/" + endpoint
     // const url = "http://localhost:5000/" + endpoint
 
